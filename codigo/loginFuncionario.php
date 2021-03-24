a525c733157f6e8bdd7418b048e46f3c4562b4e2
gabriel1og <76018617+gabriel1og@users.noreply.github.com>
2021-03-24 20:52:08 -0300

    Update loginFuncionario.php

--- codigo/loginFuncionario.php
+++ codigo/loginFuncionario.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="pt-br">
   <head>
-    <title>Oficina Alemão Alinhamento</title>
+    <title>Oficina Alemão Alinhament</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
