f74c4fe72c135abd660174c53fa5418b792314a0
gabriel1og <76018617+gabriel1og@users.noreply.github.com>
2021-03-24 20:50:36 -0300

    Update QuemSomos.php

--- codigo/QuemSomos.php
+++ codigo/QuemSomos.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br"><head>
-    <title>Oficina Alemão Alinhameno</title>
+    <title>Oficina Alemão Alinhamento</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
