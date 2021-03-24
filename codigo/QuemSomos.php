9a6cd9c9b2c7981f3dbc6a2ec72a9e1f789462fe
bruno-sclima <69166110+bruno-sclima@users.noreply.github.com>
2021-03-24 19:21:07 -0300

    Add files via upload

---
+++ codigo/QuemSomos.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="pt-br"><head>
+    <title>Oficina Alemão Alinhamento</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="qs.css">
+
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  </head>
+  <body class="bd">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+
+    <header class="header">
+        <a href="#" class="titulo">Oficina Alemão Alinhamento</a>
+        <nav>
+          <ul class="menu">
+            <li><a href="./home.php">Home</a></li>
+            <li><a href="#">Serviços</a></li>
+            <li><a href="#">Produtos</a></li>
+            <li> <a class="nav-link" href="./loginCliente.php">LogIn <i class="fas fa-sign-in-alt"></i> LogOut <i class="fas fa-sign-out-alt"></i></a></li>
+            <li><a class="btn btn-success btn-sm ml-3" href="cart.html">
+              <i class="fa fa-shopping-cart"></i> Carrinho
+              <span class="badge badge-light">3</span>
+          </a></li>
+          </ul>
+        </nav>
+      </header>
+
+      <div class="card flex-md-row">
+        <div class="card-body">
+           <strong class="d-inline-block">QUEM SOMOS</strong>
+           <p></p>
+           <p class="card-text">A Oficina Alemão Alinhamento há mais de 30 anos, em Belo Horizonte, presta serviços relacionados à
+            mecânica, tanto para veículos nacionais quanto importados, com uma oferta de serviços de qualidade
+            de reparação e consertos.</p>
+            <p class="card-text">Com isso, conquistamos uma clientela fiel ao longo dos anos e portanto, uma boa imagem no ramo. 
+            Estamos localizados na Rua do Ouro 83 no bairro Serra na região centro-sul, e esperamos por você.</p>
+        </div>
+        <img class="card-img" alt="Thumbnail [200x250]" src="slogan.jfif" style="width: 350px; height: 500px; border-radius: 10px;">
+      </div>
+
+        <footer class="footer-qs">
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto">
+            <h5>Telefone fixo <i class="fas fa-phone"></i></h5>
+            <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-25">
+            <ul class="list-unstyled">
+                <li><a href="">(31)3241-1012</a></li>
+            </ul>
+        </div>
+
+        <div class="col-md-4 col-lg-3 col-xl-5">
+            <h5>Whatsapp <i class="fab fa-whatsapp"></i></h5>
+            <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-25">
+            <ul class="list-unstyled">
+                <li>n° telefone proprietario</li>
+            </ul>
+        </div>
+        </footer>
+  </body>
+</html>
