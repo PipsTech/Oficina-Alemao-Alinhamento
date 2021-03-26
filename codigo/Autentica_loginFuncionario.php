e352198b6f9729019463e50584e789dc4a7c239b
bruno-sclima <69166110+bruno-sclima@users.noreply.github.com>
2021-03-25 22:04:05 -0300

    Add files via upload

---
+++ codigo/Autentica_loginFuncionario.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+$host = "localhost";
+$root = "root";
+$pass = "";
+$con = new mysqli($host,$root,$pass);
+if ($con -> connect_errno) {
+    echo "Failed to connect to MySQL: " . $con -> connect_error;
+    exit();
+  }
+
+
+mysqli_select_db($con, 'tis');
+
+
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+
+$comando = "SELECT Nome,Função FROM `funcionario` WHERE Email = '$email' AND Senha = '$senha'";
+$res = mysqli_query($con,$comando);
+$num = mysqli_num_rows($res);
+
+if(1 == $num){
+    echo "login realizado com sucesso\n";
+    $a = $res->fetch_array();
+    
+    echo $a['Nome'];
+    echo " - ";
+    echo $a['Função'];
+}
+else{
+    echo "login falhou";
+}
+
+
+?>
