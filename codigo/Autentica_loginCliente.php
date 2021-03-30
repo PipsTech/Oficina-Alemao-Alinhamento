678ac84ed7a7cf9eb3c1d950aa6f97e1431426cf
bruno-sclima <69166110+bruno-sclima@users.noreply.github.com>
2021-03-30 20:51:56 -0300

    Update Autentica_loginCliente.php

--- codigo/Autentica_loginCliente.php
+++ codigo/Autentica_loginCliente.php
@@ -1,33 +1,11 @@
 <?php
-session_start();
-$host = "localhost";
-$root = "root";
-$pass = "";
-$con = new mysqli($host,$root,$pass);
-if ($con -> connect_errno) {
-    echo "Failed to connect to MySQL: " . $con -> connect_error;
-    exit();
-  }
-
-
-mysqli_select_db($con, 'tis');
-
-
+require "ControleConta.php";
+$obj = new Controle();
 $email = $_POST['email'];
 $senha = $_POST['senha'];
+if($email != null || $senha!=null)
+    $id = $obj->loginCliente($email,$senha)
 
-$comando = "SELECT Nome FROM `cliente` WHERE Email = '$email' AND Senha = '$senha'";
-$res = mysqli_query($con,$comando);
-$num = mysqli_num_rows($res);
-
-if(1 == $num){
-    echo "login realizado com sucesso\n";
-    $a = $res->fetch_array();
-    echo $a['Nome'];
-}
-else{
-    echo "login falhou";
-}
 
 
 ?>
