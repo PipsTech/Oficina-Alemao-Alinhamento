b633712fd5380bedd38bfce85c4dd0d8d0815f28
bruno-sclima <69166110+bruno-sclima@users.noreply.github.com>
2021-04-15 22:17:26 -0300

    Update cadP.php

--- codigo/cadP.php
+++ codigo/cadP.php
@@ -6,5 +6,11 @@ $nome = $_POST['nome'];
 $descricao = $_POST['descricao'];
 $preco = $_POST['preco'];
 $quant = $_POST['quantidade'];
-$obj-> cadastraProduto($nome,$descricao,$preco,$quant);
+if($_POST){
+    $imageName = mysqli_real_escape_string($obj->__getcon() ,$_FILES["image"]["name"] ) ;
+    $imageType = mysqli_real_escape_string($obj->__getcon() ,$_FILES["image"]["type"] ) ;
+    $imageData = mysqli_real_escape_string($obj->__getcon() ,file_get_contents($_FILES["image"]["tmp_name"]) ) ;
+}
+
+$obj-> cadastraProduto($nome,$descricao,$preco,$quant,$imageData);
 ?>
